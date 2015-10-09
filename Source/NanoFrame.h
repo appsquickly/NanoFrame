0c3262172f9c14aef699cfd24695f5f005a16c3d
Jasper Blues <jasper@appsquick.ly>
2015-10-09 13:58:53 +0800

    headers.

--- Source/NanoFrame.h
+++ Source/NanoFrame.h
@@ -4,11 +4,9 @@
 //  Copyright 2015 AppsQuick.ly
 //  All Rights Reserved.
 //
-//  NOTICE: This software is the proprietary information of AppsQuick.ly
-//  Use is subject to license terms.
+//  NOTICE: Use is subject to license terms.
 //
 ////////////////////////////////////////////////////////////////////////////////
-
 #import "CALayer+Image.h"
 #import "UIScreen+NanoFrame.h"
 #import "UIView+NanoFrame.h"
