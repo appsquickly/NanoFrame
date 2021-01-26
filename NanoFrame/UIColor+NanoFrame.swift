348f2ee626a78c4769ee5ead86c47c856b1940be
Jasper Blues <jasper@liberation-data.com>
2021-01-26 09:49:04 +0800

    Fix gradient color crash when frame is .zero

--- NanoFrame/UIColor+NanoFrame.swift
+++ NanoFrame/UIColor+NanoFrame.swift
@@ -25,30 +25,38 @@ public extension UIColor {
 
     convenience init(hex: Int, alpha: CGFloat) {
         let components = (
-                R: CGFloat((hex >> 16) & 0xff) / 255,
-                G: CGFloat((hex >> 08) & 0xff) / 255,
-                B: CGFloat((hex >> 00) & 0xff) / 255
+            R: CGFloat((hex >> 16) & 0xff) / 255,
+            G: CGFloat((hex >> 08) & 0xff) / 255,
+            B: CGFloat((hex >> 00) & 0xff) / 255
         )
         self.init(red: components.R, green: components.G, blue: components.B, alpha: alpha)
     }
 
     convenience init(gradient: [UIColor], direction: GradientDirection = .TopToBottom, inFrame: CGRect) {
 
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = inFrame
-        gradientLayer.colors = gradient.map({ $0.cgColor })
-
-        if direction == .LeftToRight {
-            gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
-            gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        guard gradient.count >= 2 else {
+            fatalError("Gradient is useless without at least two colors")
         }
 
-        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
-        gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
+        if inFrame.size == .zero {
+            self.init(hex: 0x000000)
+        } else {
+            let gradientLayer = CAGradientLayer()
+            gradientLayer.frame = inFrame
+            gradientLayer.colors = gradient.map({ $0.cgColor })
 
-        self.init(patternImage: image!)
+            if direction == .LeftToRight {
+                gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+                gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+            }
+
+            UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+            gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+
+            self.init(patternImage: image!)
+        }
     }
 
 
